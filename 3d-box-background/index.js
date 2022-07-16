2300a6f3974b6fc42dfabed39050b0bf15525c8b
Asuka <2356380395@qq.com>
2022-07-16 09:05:04 +0800

    update fault project

--- 3d-box-background/index.js
+++ 3d-box-background/index.js
@@ -7,13 +7,13 @@ btn.addEventListener('click', () => container.classList.toggle('big'))
 
 function create3dBox() {
   // 生成行和列的box
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
       const box = document.createElement('div')
       box.classList.add('box')
       // 行：向左偏移(取负值)第j个Box 乘 Box宽度
       // 列: 向上偏移(取负值)第i个Box 乘 Box宽度
-      box.style.backgroundPosition = `${-j * 250}px ${-i * 250}px`
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
       // 添加到container容器中
       container.appendChild(box)
     }
